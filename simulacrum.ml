3d402001a54a35eb73d4664dbf8a0ba0f3f70e91
Alexey Vyskubov <tascafoo@gmail.com>
2009-11-27 20:10:13 +0200

    Started to use subdirectories

--- simulacrum.ml
+++ simulacrum.ml
@@ -1,15 +1,7 @@
 open Definitions ;;
+open Strategy ;;
 Random.self_init ()
 
-class virtual strategy =
-(* all playing strategies are inherited from this class *)
-object
-  val virtual name : string (* name of the strategy to print in results *)
-  method get_name = name
-  method virtual play : move -> move
-  (* Input: the move played by the opponent during the previous round.
-   * Output: the move to play this round. *)
-end
 
 class idiot =
 object
